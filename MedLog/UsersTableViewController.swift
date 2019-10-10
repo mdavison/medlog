41fcd20f15bf9eb7c52903a1a04276eb0f2e77f5
Morgan Davison <morgan.davison@gmail.com>
2019-10-09 20:48:09 -0400

    Add ability to delete users

--- MedLog/UsersTableViewController.swift
+++ MedLog/UsersTableViewController.swift
@@ -66,25 +66,20 @@ class UsersTableViewController: UITableViewController {
         }
     }
 
-    /*
-    // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
         return true
     }
-    */
 
-    /*
-    // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            // Delete the row from the data source
+            let user = users[indexPath.row]
+            coreDataStack?.managedContext.delete(user)
+            coreDataStack?.saveContext()
+            users.remove(at: indexPath.row)
+            
             tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
         }    
     }
-    */
 
     /*
     // Override to support rearranging the table view.
@@ -119,7 +114,8 @@ class UsersTableViewController: UITableViewController {
         
         if let user = User(name: "\(Date())", coreDataStack: coreDataStack) {
             users.append(user)
-            tableView.reloadData()
+            let indexPath = IndexPath(row: users.count - 1, section: 0)
+            tableView.insertRows(at: [indexPath], with: .automatic)
         }
     }
     
